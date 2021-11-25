ed6c3dcccea94335b4cf46958e7be974bf7aeffd
root <root@lms.devstack.edx>
2021-11-25 13:49:40 +0000

    changes in returning context

--- src/ol_openedx_rapid_response/context_api.py
+++ src/ol_openedx_rapid_response/context_api.py
@@ -1,21 +1,31 @@
 from django.utils.translation import ugettext as _
 from rapid_response_xblock.utils import get_run_data_for_course
+from opaque_keys.edx.keys import CourseKey
 
+    
+# section_key context is the name of html file in templates
+# if section_key is changed then also change the name of html file 
 
 def plugin_context(context):
     """ Provide data for the rapid responses dashboard section """
-    course_key = context["course_key"]
-
-    # section_key is the name of html file in templates
-    # if section_key is changed then also change the name of html file 
+    course = context.get("course", False)
+    sections = context.get("sections", False)
+    
+    if not (course and sections):
+        return context
+    
+    course_key = course.id
 
     rapid_response_context = {
         'section_key': 'rapid_response',
         'section_display_name': _('Rapid Responses'),
         'problem_runs': get_run_data_for_course(course_key=course_key),
         'course_key': course_key,
-        'download_url': 'get_rapid_response_report'
+        'download_url': 'get_rapid_response_report',
+        'template_path_prefix': '/'
     }
-    context["sections"].append(rapid_response_context)
+    
+    sections.append(rapid_response_context)
+    context["sections"] = sections
 
     return context
